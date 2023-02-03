0a8e913d6d1506ca8ee965c1a36ea7d0f69c1f03
Riku Brandt <brandtriku@gmail.com>
2023-02-03 11:28:34 +0200

    learn function

--- lunar.py
+++ lunar.py
@@ -51,11 +51,61 @@ class Agent():
         self.Q_eval = DQNetwork(lr=lr, n_actions=n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
 
         self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.net_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
 
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
 
     def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_count % self.mem_size # Get first memory space and wrap around.
+        # Get first memory space and wrap around.
+        index = self.mem_count % self.mem_size
+        self.state_memory[index] = state
+        self.reward_memory[index] = reward
+        self.action_memory[index] = action
+        self.terminal_memory[index] = done
+
+        self.mem_count += 1
+
+    def choose_action(self, observation):
+        # Take optimal or explor
+        if np.random() < self.epsilon:
+            state = torch.tensor([observation]).to(self.Q_eval.device)
+            actions = self.Q_eval.forward(state)
+            action = torch.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def learn(self):
+        if self.mem_count < self.batch_size:
+            return
+        self.Q_eval.optimizer.zero_grad()
+
+        max_mem = min(self.mem_count, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+        
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+
+        state_batch = torch.tensor(self.state_memory[batch]).to(self.Q_eval.device)
+        new_state_batch = torch.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+        reward_batch = torch.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
+        terminal_batch = torch.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+
+        action_batch = self.action_memory[batch]
+
+        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
+        q_next = self.Q_eval.forward(new_state_batch)
+        q_next[terminal_batch] = 0.0
+
+        q_target = reward_batch + self.gamma * torch.max(q_next, dim=1)[0]
+
+        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
+
+        loss.backward()
+
+        self.Q_eval.optimizer.step()
+
+
+
